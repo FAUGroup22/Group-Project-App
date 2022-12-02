691dd0461e834d1ceb5e2f9d8b0e0158ef35b23f
giacobn <giacobn@comcast.net>
2022-12-02 10:09:51 -0500

    working feed somewhat needs work

---
+++ ubidv2/FeedCell.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCell.swift
+//  ubidv2
+//
+//  Created by nicholas giacobbe on 12/2/22.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var titleLable: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+}
