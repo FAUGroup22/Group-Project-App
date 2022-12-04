1171cd390a365719dd8f979205c9792246412127
gbell331 <111946282+gbell331@users.noreply.github.com>
2022-12-04 13:34:09 -0500

    starting the last two screens

---
+++ ubidv2/WonItemsCell.swift
@@ -0,0 +1,23 @@
+//
+//  WonItemsCell.swift
+//  ubidv2
+//
+//  Created by Garrett Bell on 12/4/22.
+//
+
+import UIKit
+
+class WonItemsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
